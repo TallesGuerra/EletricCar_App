7af91d070a57561373e2861579f1e5ba640bc168
Talles Guerra <talles-guerra@hotmail.com>
2026-01-13 17:46:23 +0000

    Icon implemented and updated

--- app/src/main/java/com/example/eletriccar_app/MainActivity.kt
+++ app/src/main/java/com/example/eletriccar_app/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.example.eletriccar_app
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import kotlin.toString
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var precoKwh: EditText
@@ -29,7 +28,6 @@ class MainActivity : AppCompatActivity() {
         btnCalcular = findViewById(R.id.btn_calcular)
         kmPercorrido = findViewById(R.id.edt_kmPercorrido)
         resultado = findViewById(R.id.tv_resultado)
-
 
     }
 
