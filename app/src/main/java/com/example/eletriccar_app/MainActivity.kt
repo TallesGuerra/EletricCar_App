cd1e71e39e5c52f6cbe90962f0a797f35bef7504
Talles Guerra <talles-guerra@hotmail.com>
2026-01-13 17:03:53 +0000

    Resultado criado e inserido na View. Calulo do consumo criado.

--- app/src/main/java/com/example/eletriccar_app/MainActivity.kt
+++ app/src/main/java/com/example/eletriccar_app/MainActivity.kt
@@ -2,17 +2,18 @@ package com.example.eletriccar_app
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.EditText
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import kotlin.toString
 
 class MainActivity : AppCompatActivity() {
     lateinit var precoKwh: EditText
     lateinit var btnCalcular: Button
-
+    lateinit var kmPercorrido: EditText
+    lateinit var resultado: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,17 +25,29 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setupView() {
-        precoKwh = findViewById(R.id.edtit_text_precoKwh)
+        precoKwh = findViewById(R.id.edt_precoKwh)
         btnCalcular = findViewById(R.id.btn_calcular)
+        kmPercorrido = findViewById(R.id.edt_kmPercorrido)
+        resultado = findViewById(R.id.tv_resultado)
+
 
     }
 
     private fun setupListeners() {
         btnCalcular.setOnClickListener {
-            val precoKwh_informado = precoKwh.text
-            Log.d("precoKwh", precoKwh_informado.toString())
+            calcular()
 
         }
     }
 
+    fun calcular() {
+        val precoKwh_informado = precoKwh.text.toString().toFloat()
+        val kmPercorrido_informado = kmPercorrido.text.toString().toFloat()
+
+        val resultadoConsumo: Float = precoKwh_informado / kmPercorrido_informado
+
+        resultado.text = resultadoConsumo.toString()
+    }
 }
+
+
