1d803fa7f59258abc6b872fc5ad61546a47f6253
Talles Guerra <talles-guerra@hotmail.com>
2026-01-13 15:34:44 +0000

    fun setupView inplemented

--- app/src/main/java/com/example/eletriccar_app/MainActivity.kt
+++ app/src/main/java/com/example/eletriccar_app/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.eletriccar_app
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var precoKwh: EditText
+    lateinit var btnCalcular: Button
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }*/
+        setupView()
+        setupListeners()
     }
+
+    fun setupView() {
+        precoKwh = findViewById(R.id.edtit_text_precoKwh)
+        btnCalcular = findViewById(R.id.btn_calcular)
+
+    }
+
+    private fun setupListeners() {
+        btnCalcular.setOnClickListener {
+            val precoKwh_informado = precoKwh.text
+            Log.d("precoKwh", precoKwh_informado.toString())
+
+        }
+    }
+
 }
